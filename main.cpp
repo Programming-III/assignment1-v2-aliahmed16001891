630c8a90bff87a67a1c7b90776eaa0d1e6e16c75
aliahmed16001891 <ali.ahmed.16001891@outlook.com>
2025-11-11 09:47:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,27 +9,67 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+Person::Person(string name, int id){
+this->name=name;
+this->id=id;
+}
+Person:: Person(){
+name="noname";
+id=0;
+}
+Person::~Person(){
+}
+void Person::diplay(){
+cout<<"Name: "<< name<<"(ID: )"<<id<<endl;
 
-
-
+}
+string Person::getName(){
+return name;
+}
+int Person::getId(){
+return id;
+}
 
 
 
 
 
 // ==================== Student Class Implementation ====================
-
-
-
-
-
+Student:: Student(int yearLevel, string major){
+this->major=major;
+this->yearLevel=yearLevel;
+}
+Student:: Student(){
+yearLevel=0;
+major="no major";
+}
+Student::~Student(){
+}
+string Student::getMajor(){
+return major;
+}
+int Student::getYearLevel(){
+return yearLevel;
+}
 
 // ==================== Instructor Class Implementation ====================
 
-
-
-
-
+Instructor:: Instructor(string department,int experienceYears){
+this->department=department;
+this->experienceYears=experienceYears;
+}
+Instructor:: Instructor(){
+    department="no department";
+    experienceYears=0;
+}
+ Instructor::~Instructor(){
+    }
+string Instructor::getDepartment(){
+return department
+}
+int Instructor::getExperienceLevel(){
+return experienceYears;
+}
 
 // ==================== Course Class Implementation ====================
 
@@ -42,7 +82,7 @@ using namespace std;
 
 // ==================== Main Function ====================
 int main() {
-   
-    
+
+
     return 0;
 }
