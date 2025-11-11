b2393bfcfbed562a9c8c596cdc530abf836912f2
aliahmed16001891 <ali.ahmed.16001891@outlook.com>
2025-11-11 09:29:20 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,7 +3,15 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+class Instructor{
+private :
+    string department;
+    int experienceYears;
+public:
+    Instructor(string department,int experienceYears);
+    Instructor();
+    ~Instructor();
+};
 
 
 
